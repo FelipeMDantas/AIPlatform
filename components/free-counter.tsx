4e436c025cfaeca5b0984eb5916549483d199a37
FelipeMDantas <felipemdantas@terra.com.br>
2023-08-19 19:07:12 -0300

    Upgrade-related modal added to the project.

--- components/free-counter.tsx
+++ components/free-counter.tsx
@@ -1,10 +1,17 @@
 import { useEffect, useState } from "react";
+import { Card, CardContent } from "./ui/card";
+import { MAX_FREE_COUNTS } from "@/constants";
+import { Progress } from "./ui/progress";
+import { Button } from "./ui/button";
+import { Zap } from "lucide-react";
+import { useProModal } from "@/hooks/use-pro-model";
 
 interface FreeCounterProps {
   apiLimitCount: number;
 }
 
 export const FreeCounter = ({ apiLimitCount = 0 }) => {
+  const proModal = useProModal();
   const [mounted, setMounted] = useState(false);
 
   useEffect(() => {
@@ -15,5 +22,29 @@ export const FreeCounter = ({ apiLimitCount = 0 }) => {
     return null;
   }
 
-  return <div>FreeCounter</div>;
+  return (
+    <div className="px-3">
+      <Card className="bg-white/10 border-0">
+        <CardContent className="py-6">
+          <div className="text-center text-sm text-white mb-4 space-y-2">
+            <p>
+              {apiLimitCount} / {MAX_FREE_COUNTS} Free Generations
+            </p>
+            <Progress
+              className="h-3"
+              value={(apiLimitCount / MAX_FREE_COUNTS) * 100}
+            />
+          </div>
+          <Button
+            className="w-full"
+            variant="premium"
+            onClick={proModal.onOpen}
+          >
+            Upgrade
+            <Zap className="w-4 h-4 ml-2 fill-white" />
+          </Button>
+        </CardContent>
+      </Card>
+    </div>
+  );
 };
